11cad87caf564bafe68db2d176c9b698764ea0fb
carlosmortari <113060314+carlosmortari@users.noreply.github.com>
2023-05-09 11:14:49 -0300

    desafio + exercicio

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+    console.log(`a velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { // Condição simples
+    console.log(`Voce ultrapassou a velocidade permitida (MULTADO)`)
+} 
+console.log(`Dirija sempre com o cinto de segurança`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log(`Vivendo em ${pais} `)
+    if (pais == 'brasil') {
+        console.log(` voce é Brasileiro`)
+    } else {
+        console.log(`Estrangeiro`)
+    }
