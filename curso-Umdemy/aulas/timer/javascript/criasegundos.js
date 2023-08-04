7d0e6c17d02fce03885c7f632441bff939f2b18f
carlosmortari <113060314+carlosmortari@users.noreply.github.com>
2023-08-04 20:06:50 -0300

    terminado

--- curso-Umdemy/aulas/timer/javascript/criasegundos.js
+++
@@ -1,8 +0,0 @@
-export function criaHoraDosSegundos(segundos){
-    const data = new Date(segundos * 1000);
-        return data.toLocaleTimeString('pt-BR' , {
-            hour12: false,
-            timeZone:'UTC'
-        });
-}
-
--- curso-Umdemy/aulas/timer/javascript/iniciarelogio.js
+++
@@ -1,6 +0,0 @@
-export function iniciaRelogio(){
-    timer = setInterval(function(){
-        segundos++;
-        relogio.innerHTML = criaHoraDosSegundos(segundos)
-    }, 1000)
-}
